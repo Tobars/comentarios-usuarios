380e653f186379cfaba6ec7cea41776addc555ab
Tobar <pe.tobar@icloud.com>
2024-12-09 10:56:19 -0600

    test3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { AllPostComponent } from './all-post/all-posts.component';
 import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
+  {path: '', component : AllPostComponent},
   { path: 'all-posts', component: AllPostComponent },
   { path: 'post-details/:id', component: PostDetailsComponent },  // Ruta con parámetro :id
   { path: '**', component: ErrorComponent } // Ruta de error para manejar rutas no encontradas
