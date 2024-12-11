3a1635873104377966c3fce1b1c44c572e88c53e
Tobar <pe.tobar@icloud.com>
2024-12-11 09:16:42 -0600

    cambios

--- src/app/all-post/all-posts.component.ts
+++ src/app/all-post/all-posts.component.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';  // Para hacer peticiones HTTP
 import { CommonModule } from '@angular/common'; // Para usar directivas comunes como ngFor
 import { RouterModule } from '@angular/router'; // Para routerLink
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap'; // Usamos el modal de Bootstrap
 
 @Component({
   selector: 'app-all-posts',
@@ -16,9 +15,8 @@ export class AllPostComponent implements OnInit {
   posts: any[] = [];
   userEmail: string = '';   // Variable para almacenar el correo
   emailDisplay: string = ''; // Variable para mostrar el correo en la interfaz
-  postIdToDelete: number | null = null; // Guardamos el ID del post a eliminar
 
-  constructor(private router: Router, private http: HttpClient, private modalService: NgbModal) { }
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(): void {
     const userData = localStorage.getItem('user');
@@ -35,7 +33,6 @@ export class AllPostComponent implements OnInit {
       this.emailDisplay = this.userEmail;
     }
 
-    // Cargar posts
     this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts')
       .subscribe(data => {
         this.posts = data;
@@ -47,30 +44,8 @@ export class AllPostComponent implements OnInit {
     this.router.navigate(['/login']); // Redirigimos al login
   }
 
-  confirmDelete(postId: number): void {
-    this.postIdToDelete = postId; // Guardamos el ID del post a eliminar
-    const modal = new window.bootstrap.Modal(document.getElementById('confirmDeleteModal') as HTMLElement);
-    modal.show(); // Mostramos el modal
-  }
-
   deletePost(postId: number): void {
-    if (postId) {
-      // Realizamos la solicitud DELETE a la API
-      this.http.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-        .subscribe({
-          next: () => {
-            // Si la eliminación fue exitosa, actualizamos la tabla
-            this.posts = this.posts.filter(post => post.id !== postId);
-            // Cerramos el modal
-            const modal = new window.bootstrap.Modal(document.getElementById('confirmDeleteModal') as HTMLElement);
-            modal.hide();
-          },
-          error: (err) => {
-            alert('Error al eliminar el post.');
-            console.error(err);
-          }
-        });
-    }
+    this.posts = this.posts.filter(post => post.id !== postId);
   }
 
   addPost(): void {
